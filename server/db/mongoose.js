a95e0e75b295ed7ef8e83353f542d4199762face
Fernan de Dios <info@mediagramstudios.com>
2017-08-29 14:16:19 +0800

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 let mongoose = require('mongoose'); // DO NOT USE const
 
 mongoose.Promise = global.Promise; // configure mongoose to use promises
-mongoose.connect('mongodb://heroku:heroku@ds163613.mlab.com:63613/todo-api-heroku');
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+// workaround! base prod or dev by PORT env var, since mlab was configured outside heroku
+const mongo_uri = process.env.PORT !== undefined ? 'mongodb://heroku:heroku@ds163613.mlab.com:63613/todo-api-heroku' : 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(mongo_uri);
 
 module.exports = { mongoose };
