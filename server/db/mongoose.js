17cc195f0a443ff634b0b28ae0a23d417b4871d9
Fernan de Dios <info@mediagramstudios.com>
2017-08-29 13:17:57 +0800

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 let mongoose = require('mongoose'); // DO NOT USE const
 
 mongoose.Promise = global.Promise; // configure mongoose to use promises
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://heroku:heroku@ds163613.mlab.com:63613/todo-api-heroku');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
