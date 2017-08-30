f8b01f7dd0d732d41c55998c9b85fa84150cdbc4
Fernan de Dios <info@mediagramstudios.com>
2017-08-30 12:34:57 +0800

    Add tests for PATCH /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,7 +7,7 @@ const { Todo } = require('./../models/todo');
 
 const todos = [
   { text: 'First test todo', _id: new ObjectID() },
-  { text: 'Second test todo', _id: new ObjectID() }
+  { text: 'Second test todo', _id: new ObjectID(), completed: true, completedAt: 333 }
 ];
 
 // testing lifecycle method that runs before any test case
@@ -144,4 +144,40 @@ describe('DELETE /todos/:id', () => {
       .expect(404)
       .end(done);
   });
-})
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update todo', (done) => {
+    const hexId = todos[0]._id.toHexString();
+    const text = 'Test todo text';
+    const completed = true;
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({ text, completed })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.completed).toBe(completed);
+        expect(res.body.todo.completedAt).toBeA('number');
+        expect(res.body.todo.text).toBe(text);
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    const hexId = todos[1]._id.toHexString();;
+    const text = 'Test todo text2';
+    const completed = false;
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({ text, completed })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.completed).toBe(completed);
+        expect(res.body.todo.completedAt).toNotExist();
+        expect(res.body.todo.text).toBe(text);
+      })
+      .end(done);
+  });
+});
