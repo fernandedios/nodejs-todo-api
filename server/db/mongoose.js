9dc7d5d6c066f3d4c0d22a3fd69503c5f1631cb1
Fernan de Dios <info@mediagramstudios.com>
2017-08-30 12:54:47 +0800

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,6 @@
 let mongoose = require('mongoose'); // DO NOT USE const
 
 mongoose.Promise = global.Promise; // configure mongoose to use promises
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
-// workaround! base prod or dev by PORT env var, since mlab was configured outside heroku
-const mongo_uri = process.env.PORT !== undefined ? 'mongodb://heroku:heroku@ds163613.mlab.com:63613/todo-api-heroku' : 'mongodb://localhost:27017/TodoApp';
-mongoose.connect(mongo_uri);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
